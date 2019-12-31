e56f3e1adf7fa52a2ffa557abd89f4d00b43b482
Rakshith <rakshith@capdice.com>
2019-12-31 16:07:11 +0530

    changed index file

--- platoos v1/src/index.js
+++ platoos v1/src/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import Cart from "./components/Cart";
-import Discounts from "./components/Discounts";
+// import Cart from "./components/Cart";
+// import Discounts from "./components/Discounts";
 
-// ReactDOM.render(
-//   <BrowserRouter>
-//     <App />
-//   </BrowserRouter>,
-//   document.getElementById("root")
-// );
-ReactDOM.render(<Cart />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
+// ReactDOM.render(<Cart />, document.getElementById("root"));
 // ReactDOM.render(<Discounts />, document.getElementById("root"));
