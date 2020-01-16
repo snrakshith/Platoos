7f5dcd48a4bfa196d2ac9b64fa28645565738944
Rakshith <snrakshith.97@gmail.com>
2020-01-16 16:45:19 +0530

    modified index.js

--- platoos v2/src/index.js
+++ platoos v2/src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import Hover from "../src/screens/home/section4/components/hover/Hover";
+// import Hover from "../src/screens/home/section4/components/hover/Hover";
 
-// ReactDOM.render(
-//   <BrowserRouter>
-//     <App />
-//   </BrowserRouter>,
-//   document.getElementById("root")
-// );
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
-ReactDOM.render(<Hover />, document.getElementById("root"));
+// ReactDOM.render(<Hover />, document.getElementById("root"));
